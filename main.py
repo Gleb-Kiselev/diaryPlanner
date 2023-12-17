9da971126f04a9cbdf66e196c1f6ff785e3751be
LizaLeboshchina <90772887+LizaLeboshchina@users.noreply.github.com>
2023-12-17 16:39:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,47 +4,71 @@ from datetime import datetime, date
 import time
 import conversation
 import threading
+import markdown
 
 
 token = '6816422069:AAEG4Q1QrTZrrmldB2F5IHrMjHnU1PSgRII'
 
 task_manager = dict()
 
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(token, parse_mode="MARKDOWN")
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def start(message):
     task_manager[message.chat.id] = conversation.TaskManager()
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("Записать новое дело")
-    btn2 = types.KeyboardButton("Удалить дело")
+    btn2 = types.KeyboardButton("Получить список всех дел")
     btn3 = types.KeyboardButton("Получить дела на сегодня")
     btn4 = types.KeyboardButton("Получить дела на завтра")
-    markup.add(btn1, btn2, btn3, btn4)
-    bot.send_message(message.chat.id, text="Привет! Я тестовый бот".format(message.from_user), reply_markup=markup)
+    btn5 = types.KeyboardButton("Удалить дело")
+
+    markup.add(btn1, btn3, btn2, btn4, btn5)
+    with open('start_message', 'r')  as file:
+        text = file.read()
+    bot.send_message(message.chat.id, text, reply_markup=markup) #format(message.from_user)
 
 
 @bot.message_handler(content_types=['text'])
 def func(message):
     if (message.text == "Записать новое дело"):
 
-        mesg = bot.send_message(message.chat.id, text='''Запишите дело в формате: Время Дело
-            Например: "сегодня в 15:00 сходить к врачу"
-            Или: "2023/12/18 16:00 сделать уборку"''')
+        mesg = bot.send_message(message.chat.id, text="Введите, что вам нужно сделать и когда")
         bot.register_next_step_handler(mesg, adding_task)
-    elif (message.text == "Получить дела на сегодня"):
+    elif message.text == "Получить дела на сегодня":
 
         tasks_list = task_manager[message.chat.id].get_tasks_for_today()
         today_tasks = '\n'.join(map(str, tasks_list))
         bot.send_message(message.chat.id,
                          today_tasks if len(today_tasks) > 0 else "На сегодня ничего не запланировано.")
-    elif (message.text == "Получить дела на завтра"):
+
+    elif message.text == "Получить список всех дел":
+        tasks_list = task_manager[message.chat.id].get_all_tasks()
+        all_tasks = '\n'.join(map(str, tasks_list))
+        bot.send_message(message.chat.id,
+                         all_tasks if len(all_tasks) > 0 else "Ничего не запланировано")
+
+    elif message.text == "Получить дела на завтра":
         tasks_list = task_manager[message.chat.id].get_tasks_for_tomorrow()
         today_tasks = '\n'.join(map(str, tasks_list))
         bot.send_message(message.chat.id, today_tasks if len(today_tasks) > 0 else "На завтра ничего не запланировано.")
-    elif (message.text == "Удалить дело"):
-        bot.send_message(message.chat.id, text="Пока не умею, но скоро научусь")
+
+    elif message.text == "Удалить дело":
+
+
+        tasks_list = list(map(str, task_manager[message.chat.id].get_all_tasks()))
+        all_tasks = ''
+        for count, elem in enumerate(tasks_list):
+            all_tasks += (f'{count + 1}. {elem} \n')
+
+        if len(tasks_list) > 0:
+            bot.send_message(message.chat.id,
+                         all_tasks)
+            mesg = bot.send_message(message.chat.id, text="Введите номер задачи, которую нужно удалить")
+            bot.register_next_step_handler(mesg, deleting_task)
+        else:
+            bot.send_message(message.chat.id, "Ничего не запланировано")
     else:
         bot.send_message(message.chat.id, text="Выберите одну из предложенных команд")
 
@@ -58,6 +82,14 @@ def adding_task(message):
         task_manager[message.chat.id].add_task(task)
         bot.send_message(message.chat.id, text="Дело успешно добавлено")
 
+
+def deleting_task(message):
+    tasks_list = task_manager[message.chat.id].get_all_tasks()
+    if int(message.text) - 1 > len(tasks_list):
+        raise ValueError
+    else:
+        task_manager[message.chat.id].remove_task(int(message.text) - 1)
+        bot.send_message(message.chat.id, text="Дело успешно удалено")
 
 class ReminderHandler():
     def __init__(self):
